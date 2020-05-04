2d8f923948ab6955aa680d4ffb3e9bec55495197
pankaj4653 <pankaj.k4653@gmail.com>
2020-05-04 20:33:58 +0530

    Adding pyc files

---
+++ djangonautics/djangonautics/email_info.py
@@ -0,0 +1,14 @@
+import os
+
+
+
+#gmail_user = os.environ.get('GMAIL_USER')
+#gmail_pass = os.environ.get('GMAIL_PASS')
+
+
+
+EMAIL_USE_TLS = True
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST_USER = 'pankaj.k3353@gmail.com'
+EMAIL_HOST_PASSWORD = 'oytazrrolwfqqott'
+EMAIL_PORT = 587
